165c502213e7fd9dd7ad1903ebf537f4b0172e3c
iki1uc <entertrainer@web.de>
2026-08-24 15:13:48 +0200

    Update vector-system.js

--- vector-system.js
+++ vector-system.js
@@ -1,7 +1,7 @@
-import { QI_IQQ } from './qi-iqq.js';
+import { MIND } from './qi-iqq.js';
 
 VECTOR_SYSTEM.transfer = function() {
     const status = this.status();
     const vektoren = status.vektoren;
-    return QI_IQQ.absorb(vektoren);
+    return MIND.absorb(vektoren);
 };
