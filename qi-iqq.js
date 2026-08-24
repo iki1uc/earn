54a2cfc18ada5d35008f62249daaa850b9602626
iki1uc <entertrainer@web.de>
2026-08-24 15:07:15 +0200

    Create qi-iqq.js

---
+++ qi-iqq.js
@@ -0,0 +1,25 @@
+vexport const QI_IQQ = {
+    qi: 50,          // Lebensenergie
+    iqq: 0.50,       // Intelligenz-Qualität-Quotient
+    flow: 0.50,      // Energiefluss
+    stabilität: 0.50,
+
+    // Energie aus VECTOR SYSTEM übernehmen
+    absorb(vektoren) {
+        const sum = Object.values(vektoren).reduce((a, b) => a + b, 0);
+        this.qi = Math.min(100, this.qi + sum * 2);
+        this.iqq = Math.min(1, this.iqq + sum * 0.01);
+        this.flow = Math.min(1, this.flow + sum * 0.02);
+        this.stabilität = 1 - Math.abs(this.flow - this.iqq);
+        return this.status();
+    },
+
+    status() {
+        return {
+            qi: this.qi,
+            iqq: this.iqq,
+            flow: this.flow,
+            stabilität: this.stabilität
+        };
+    }
+};
